a9527556cbd49361e57f834a5b62e93520328bc4
MattYu <matthew.ming@spaceconcordia.ca>
2017-07-01 17:58:48 -0400

    Update QuickUnion.java

--- QuickUnion.java
+++ QuickUnion.java
@@ -3,7 +3,7 @@ package QuickUnion;
 public class QuickUnion {
 	/*
 	Creates mathematical sets of N integers with M unions where M is a nonnegative integer <=N-1
-	Use for modeling and solving scalable Percolation problems (electric conductivity, path flow, social media connections, etc.) in the most efficient mathematically optimized way.
+	Used for modeling and solving scalable Percolation problems (electric conductivity, path flow, social media connections, etc.) in the most efficient mathematically optimized way.
 	
 	Has the following methods  
 	- CreateSets(int N): create a set of N integers that are initially not linked. 
@@ -18,8 +18,8 @@ public class QuickUnion {
 	
 	Weighted tree approach with path compression as described in Sedgewick and Al. Princeton University Press: 
 	
-	- Each object contains 2 array: id[] and size[]. 
-	- Abstractly, id[] is used to represent the roots and children of a tree.	If id[i] = i, then id[i] is a root. If id[i] is a children, id[i] = the index of its root in id[].
+	- Each object contains 2 arrays: id[] and size[]. 
+	- Abstractly, id[] is used to represent the roots and children of a tree. If id[i] = i, then id[i] is a root. If id[i] is a children, id[i] = the index of its root in id[].
 	- size[] keeps track of the number of elements a tree/set contains. 
 	- When an object of class is initialized, all elements are initially unlinked. Thus id[i] is set initially to = i for all i in N. Likewise, size[i] = 1.
 	- When Union is performed, the root of the smallest tree is linked to the root of the largest tree (larger = container more elements)
